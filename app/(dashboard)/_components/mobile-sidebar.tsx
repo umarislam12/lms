115a36a56918684f83dba61ad5a0d308e9cbd30a
Umar <f14b150@ibitpu.edu.pk>
2023-09-28 15:45:10 +0500

    Sidebar,auth,partial navbar added

---
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,8 @@
+import { Menu } from "lucide-react";
+
+
+const MobileSidebar = () => {
+    return ( <Menu/> );
+}
+ 
+export default MobileSidebar;
---
+++ app/test/page.tsx
@@ -0,0 +1,5 @@
+const TestPage = () => {
+    return ( <div>This is unprotected page</div> );
+}
+ 
+export default TestPage;
